5fe8ecf70f7fa4afbe590df6d8ad0715afd6c457
Jay Gonzales <jaygonzales@mac.com>
2016-10-19 21:40:44 -0700

    add Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,11 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # For Devise - Ensure you have defined default url options in your environments files. Here
+  # is an example of default_url_options appropriate for a development environment
+  # in config/environments/development.rb. 
+  # In production, :host should be set to the actual host of your application.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
