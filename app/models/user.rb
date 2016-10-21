9148585fccf623926905f016bb1741df2cf309f4
Jay Gonzales <jaygonzales@mac.com>
2016-10-21 01:18:16 -0700

    add signin signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # :confirmable, :lockable, :timeoutable, :recoverable, and :omniauthable
+  devise :database_authenticatable, :registerable, 
+  :rememberable, :trackable, :validatable
 end
