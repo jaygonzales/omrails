18b1b090327f8b4ceafeee71279df888c5e49700
Jay Gonzales <jaygonzales@mac.com>
2016-10-28 00:08:34 -0700

    add admin ui

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -13,5 +13,22 @@ ActiveAdmin.register User do
 #   permitted
 # end
 
+	permit_params :email, :admin
+
+	form do |f|
+		f.semantic_errors
+		f.inputs do
+			f.input :email
+			f.input :admin
+		end
+		f.actions
+	end
+
+	index do
+		id_column
+		column :email
+		column :admin
+		actions
+	end
 
 end
