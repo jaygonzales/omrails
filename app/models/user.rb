f67dd0d4dd2ff46abff2557a6aaa53cf73e3450f
Jay Gonzales <jaygonzales@mac.com>
2016-10-30 20:39:33 -0700

    Tweets admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :recoverable, and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
+
+	has_many :tweets
 end
